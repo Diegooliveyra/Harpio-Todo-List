4fa3256d36353e7f8d78b6b7f28aced79270bf11
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-12-09 14:55:41 -0300

    feat: button component

---
+++ src/Components/Button/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { ReactNode } from 'react';
+import * as S from './styles';
+
+export type ThemeButton = 'primary' | 'danger';
+
+type ButtonProps = React.ComponentProps<'button'> & {
+  children: ReactNode;
+  disabled?: boolean;
+  theme?: ThemeButton;
+};
+const Button = ({
+  children,
+  disabled = false,
+  theme = 'primary',
+  ...rest
+}: ButtonProps) => {
+  return (
+    <S.Container themeBtn={theme} disabled={disabled} {...rest}>
+      {children}
+    </S.Container>
+  );
+};
+
+export default Button;
---
+++ src/Components/Button/styles.ts
@@ -0,0 +1,55 @@
+'use client';
+
+import theme from '@/styles/theme';
+import styled, { css } from 'styled-components';
+import { ThemeButton } from '.';
+
+const themeModifier = {
+  primary: () => css`
+    background-color: ${theme.colors.green};
+    color: ${theme.colors.white};
+    border: 1px solid transparent;
+
+    &:hover {
+      filter: brightness(1.1);
+    }
+  `,
+  danger: () => css`
+    background-color: transparent;
+    color: ${theme.colors.white};
+
+    &:hover {
+      filter: brightness(1.1);
+    }
+  `,
+};
+
+type ContainerProps = {
+  disabled: boolean;
+  themeBtn: ThemeButton;
+};
+
+export const Container = styled.button<ContainerProps>`
+  ${({ disabled, themeBtn }) => css`
+    display: flex;
+    align-items: center;
+    gap: ${theme.spacings.xxsmall};
+    font-size: ${theme.font.sizes.body};
+    font-weight: ${theme.font.normal};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xsmall};
+
+    border: none;
+    border-radius: ${theme.border.radius};
+    width: 100%;
+    transition: all 0.4s ease-in-out;
+
+    cursor: ${disabled ? 'not-allowed' : 'pointer'};
+    opacity: ${disabled ? '.3' : '1'};
+
+    ${theme && themeModifier[themeBtn]}
+
+    svg {
+      margin-top: 2px;
+    }
+  `}
+`;
