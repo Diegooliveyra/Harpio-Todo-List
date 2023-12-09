87e7c50db20fd9efa698724d519f50d57f063dd3
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-12-09 15:28:48 -0300

    feat: create ultis for texts

---
+++ src/ultis/texts/index.ts
@@ -0,0 +1,17 @@
+type textTruncatProps = {
+  text: string;
+  size: number;
+};
+
+export function textTrucante({ text, size }: textTruncatProps) {
+  if (text.length > size) {
+    return text.slice(0, size) + '...';
+  }
+
+  return text;
+}
+
+export function capitalizeFirstLetter(str: string): string {
+  const lowerCase = str.toLowerCase();
+  return lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+}
