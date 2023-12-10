d3568f01fc59b7655a972c875e20647f02cc6ded
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-12-10 15:55:30 -0300

    feat: create apis

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,105 @@
+import { connectToDatabase } from '@/helpers/server-helpers';
+import { NextResponse } from 'next/server';
+
+import { StatusEnum } from '@prisma/client';
+import prisma from '../../../../../prisma';
+
+export const GET = async (
+  request: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params;
+    await connectToDatabase();
+
+    const task = await prisma.task.findUnique({
+      where: { id },
+    });
+
+    if (!task) {
+      return NextResponse.json({ message: 'Task not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(task, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const PUT = async (
+  req: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) => {
+  try {
+    const { id } = params;
+    const body = await req.json();
+    if (!body || Object.keys(body).length === 0) {
+      return NextResponse.json(
+        { message: 'Invalid request body' },
+        { status: 400 }
+      );
+    }
+
+    await connectToDatabase();
+
+    const existingTask = await prisma.task.findUnique({
+      where: { id },
+    });
+
+    if (!existingTask) {
+      return NextResponse.json({ message: 'Task not found' }, { status: 404 });
+    }
+
+    const updatedTask = await prisma.task.update({
+      where: { id },
+      data: body,
+    });
+
+    return NextResponse.json(updatedTask, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const DELETE = async (
+  request: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params;
+
+    await connectToDatabase();
+
+    const existingTask = await prisma.task.findUnique({
+      where: { id },
+    });
+
+    if (!existingTask) {
+      return NextResponse.json({ message: 'Task not found' }, { status: 404 });
+    }
+
+    await prisma.task.delete({
+      where: { id },
+    });
+
+    return NextResponse.json(
+      { message: 'Task deleted successfully' },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,83 @@
+import { connectToDatabase } from '@/helpers/server-helpers';
+import { NextResponse } from 'next/server';
+import prisma from '../../../../prisma';
+import { StatusEnum } from '@prisma/client';
+
+export const POST = async (req: Request) => {
+  try {
+    const { description, status } = await req.json();
+
+    if (!description || !status) {
+      return NextResponse.json({ message: 'Invalid data', status: 422 });
+    }
+
+    await connectToDatabase();
+
+    const typedStatus: StatusEnum = status;
+
+    const task = await prisma.task.create({
+      data: {
+        description,
+        status: typedStatus,
+      },
+    });
+
+    return NextResponse.json(task, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const GET = async (req: Request) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const status = searchParams.get('status') as StatusEnum;
+    const page = searchParams.get('page');
+    const size = searchParams.get('size');
+
+    await connectToDatabase();
+
+    const queryOptions: {
+      where?: { status: StatusEnum };
+      skip?: number;
+      take?: number;
+    } = {};
+
+    if (status) {
+      queryOptions.where = { status: StatusEnum[status] };
+    }
+
+    if (page && size) {
+      queryOptions.skip = (+page - 1) * +size;
+      queryOptions.take = +size;
+    }
+
+    const [tasks, totalTasks] = await Promise.all([
+      prisma.task.findMany(queryOptions),
+      prisma.task.count({
+        where: { status: StatusEnum[status] },
+      }),
+    ]);
+
+    const totalPages = Math.ceil(totalTasks / +size!);
+
+    return NextResponse.json(
+      {
+        content: tasks,
+        totalElements: totalTasks,
+        totalPages,
+        pageSize: +size!,
+        currentPage: +page!,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
